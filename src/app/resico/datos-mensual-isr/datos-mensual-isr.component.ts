caa59d4f97fde7bd5614356d1c1dd4e1ce2c7606
Fernando Ortiz <chistianfernando321@gmail.com>
2024-12-08 22:34:20 -0600

    Cambios en los datos necesarios a guardar, los datos necesarios estan en el componente datos-mensual-iva

--- src/app/resico/datos-mensual-isr/datos-mensual-isr.component.ts
+++ src/app/resico/datos-mensual-isr/datos-mensual-isr.component.ts
@@ -1,26 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MensualIsrTransferService } from '../../shared/mensual-isr-transfer/mensual-isr-transfer.service';
+import { MensualIvaTransferService } from '../../shared/mensual-iva-transfer/mensual-iva-transfer.service';
 
 @Component({
   selector: 'app-datos-mensual-isr',
   templateUrl: './datos-mensual-isr.component.html',
   styleUrl: './datos-mensual-isr.component.css'
 })
-export class DatosMensualIsrComponent {
+export class DatosMensualIsrComponent implements OnInit{
   cantidadAPagar: number = 0;
   isrRetenido: number = 0;
+  cantidadAPagarIVA: number = 0;
 
-  constructor(private transferService: MensualIsrTransferService) {}
+  constructor(private transferServiceISR: MensualIsrTransferService,
+              private transferServiceIVA: MensualIvaTransferService
+  ) {}
 
   ngOnInit(): void {
     // Escuchar el valor de "Cantidad a pagar"
-    this.transferService.cantidadAPagar$.subscribe((value) => {
+    this.transferServiceISR.cantidadAPagar$.subscribe((value) => {
       this.cantidadAPagar = value;
     });
 
     // Escuchar el valor de "ISR retenido por personas morales"
-    this.transferService.isrRetenido$.subscribe((value) => {
+    this.transferServiceISR.isrRetenido$.subscribe((value) => {
       this.isrRetenido = value;
+    });
+
+    // Escuchar el valor de "Cantidad a pagar iva"
+    this.transferServiceIVA.cantidadAPagar$.subscribe((value) => {
+      this.cantidadAPagarIVA = value;
     });
   }
 
--- src/app/resico/escoger-declaracion/escoger-declaracion.component.ts
+++ src/app/resico/escoger-declaracion/escoger-declaracion.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MensualIsrTransferService } from '../../shared/mensual-isr-transfer/mensual-isr-transfer.service';
+import { MensualIvaTransferService } from '../../shared/mensual-iva-transfer/mensual-iva-transfer.service';
 
 @Component({
   selector: 'app-escoger-declaracion',
   templateUrl: './escoger-declaracion.component.html',
   styleUrl: './escoger-declaracion.component.css'
 })
-export class EscogerDeclaracionComponent {
+export class EscogerDeclaracionComponent implements OnInit {
+  cantidadAPagarISR: number = 0; // Para "Cantidad a pagar" del ISR
+  cantidadAPagarIVA: number = 0; // Para "Cantidad a pagar" del IVA
+  isrRetenido: number = 0; // Para "ISR retenido por personas morales"
 
-  
+  constructor(
+    private isrTransferService: MensualIsrTransferService,
+    private ivaTransferService: MensualIvaTransferService
+  ) {}
 
+  ngOnInit(): void {
+    // Escuchar el valor de "Cantidad a pagar" para ISR
+    this.isrTransferService.cantidadAPagar$.subscribe((value) => {
+      this.cantidadAPagarISR = value;
+    });
+
+    // Escuchar el valor de "ISR retenido por personas morales"
+    this.isrTransferService.isrRetenido$.subscribe((value) => {
+      this.isrRetenido = value;
+    });
+
+    // Escuchar el valor de "Cantidad a pagar" para IVA
+    this.ivaTransferService.cantidadAPagar$.subscribe((value) => {
+      this.cantidadAPagarIVA = value;
+    });
+  }
 }
